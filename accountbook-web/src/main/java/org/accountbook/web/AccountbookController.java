55538aa4c9006043ca24eb15cb048f03703d389c
Marc Weinberger <marc.weinberger@me.com>
2009-02-10 22:25:22 +0100

    initial working (g st) version of spring-dm webmodule

--- accountbook-web/src/main/java/org/accountbook/web/AccountbookController.java
+++ accountbook-web/src/main/java/org/accountbook/web/AccountbookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AccountbookController {
 
-	@RequestMapping("/account.html")
+	@RequestMapping("/account.htm")
 	public String account() {
 		return "account";
 	}
