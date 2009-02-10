a9d1f20c3a8122e4dca2ae85ca13701bdb4399d0
Marc Weinberger <marc.weinberger@me.com>
2009-02-10 23:19:03 +0100

    added AccounbookControllerTest

--- accountbook-web/src/main/java/org/accountbook/web/AccountbookController.java
+++ accountbook-web/src/main/java/org/accountbook/web/AccountbookController.java
@@ -1,14 +1,15 @@
 package org.accountbook.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class AccountbookController {
 
 	@RequestMapping("/account.htm")
-	public String account() {
+	public String account(ModelMap model) {
+		model.addAttribute("name", "account name");
 		return "account";
 	}
 }
